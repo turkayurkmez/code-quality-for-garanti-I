8d9971f460d114e99432a9084f1df86a639898ff
türkay ürkmez <turkay.urkmez@outlook.com>
2025-05-15 17:39:25 +0300

    #
    Add CommandPattern project and implement Command Pattern
    
    This commit introduces a new "CommandPattern" project to the
    SomeDesignPatterns solution, including a new project file
    `CommandPattern.csproj`. The `DP.cs` file is updated to define
    the `ICommand` interface and implement command classes for
    creating and discounting products. A `CommandInvoker` class
    is added to manage command execution, and `Program.cs` is
    updated to demonstrate the usage of the Command Pattern.

---
+++ SomeDesignPatterns/CommandPattern/DP.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommandPattern
+{
+   //1. Tüm komutların ortak olmasını sağla:
+   public interface ICommand
+    {
+        //Tavsiye: komutları takip edin.
+        public int Id { get; set; }
+        void Execute();
+    }
+
+    //2. Komutları kim çalıştıracak? - AŞÇI kim?
+
+    public class DbCommandReceiver 
+    {
+        public void CreateNewProduct(string name, double price)
+        {
+            Console.WriteLine("Ekledim");
+        }
+
+        public void DiscountProductPrice(string name, double price) {
+            Console.WriteLine("Fiyatta indirim yapıldı");
+        }
+
+    }
+
+    //3. Komutlarınızı oluşturun:
+
+    public class CreateNewProductCommand : ICommand
+    {
+        public int Id { get; set; }
+
+        //Her komut, alıcısını tanımalı.
+
+        DbCommandReceiver receiver;
+
+        public CreateNewProductCommand(DbCommandReceiver receiver)
+        {
+            this.receiver = receiver;
+        }
+
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public void Execute()
+        {
+            receiver.CreateNewProduct(Name, Price); 
+        }
+    }
+
+    public class DiscountProductPrice : ICommand
+    {
+        private DbCommandReceiver receiver;
+
+        public DiscountProductPrice(DbCommandReceiver receiver)
+        {
+            this.receiver = receiver;
+        }
+
+        public int Id { get; set ; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public void Execute()
+        {
+            receiver.DiscountProductPrice(Name, Price); 
+        }
+    }
+
+
+    public class CommandInvoker
+    {
+        private  Queue<ICommand> commands = new();
+
+        public void AddCommand(ICommand command)
+        {
+            commands.Enqueue(command);
+        }
+
+        public void Clear() => commands.Clear();
+        public void Remove(ICommand command) => commands = new Queue<ICommand>(commands.Where(c => c != command));
+
+        public void ExecuteCommand()
+        {
+            while (commands.Count>0)
+            {
+                var command = commands.Dequeue();
+                command.Execute();
+            }
+        }
+
+    }
+}
---
+++ SomeDesignPatterns/CommandPattern/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace CommandPattern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            /*
+             * Belirli bir kaynak (db, api, excel dosyası vs) üzerinde gerçekleşecek olan komutları nasıl daha muntazam yönetirsiniz?
+             * 
+             * 
+             */
+
+            DbCommandReceiver dbCommandReceiver = new DbCommandReceiver();
+            CreateNewProductCommand createNewProductCommand = new CreateNewProductCommand(dbCommandReceiver)
+            {
+                Name ="Test",
+                Price = 1
+
+            };
+
+            DiscountProductPrice discountProductPrice = new DiscountProductPrice(dbCommandReceiver)
+            {
+                Name = "Test",
+                Price = 1
+            };
+
+            CommandInvoker commandInvoker = new CommandInvoker();
+            commandInvoker.AddCommand(createNewProductCommand);
+            commandInvoker.AddCommand(discountProductPrice);
+
+            commandInvoker.ExecuteCommand();
+
+        }
+    }
+}
